c88776e4a7969f5fc2c3eccd5560a6bc5ec45853
HectorSH-3 <anonymousred343@gmail.com>
2022-09-19 12:04:04 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["graphicsfamily.com", "cdn.sanity.io", "lh3.googleusercontent.com"], formats: ['image/avif', 'image/webp'],
+  },
+};
+
